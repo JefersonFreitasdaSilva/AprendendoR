736f713ed754ca04fa1d3378a26ceaf3a6f95565
jeferson freitas da silva <98712214+jefersonfreitas2525@users.noreply.github.com>
2023-03-28 21:48:59 -0300

    Add files via upload

---
+++ Operadores Relacionais.R
@@ -0,0 +1,13 @@
+X = 5
+Y = 2
+#respostas bollean
+
+print (x > y); #maior
+print (x < y); #menor
+print (x >= y); #maior ou igual
+print (x <= y); #menor ou igual
+print (x == y); #igual
+print (x != y); #diferente
+
+print(5 == "5") #true(compara o conteudo)
+print(5L == "5L") #FALSE
---
+++ Operadores_relacionais2.R
@@ -0,0 +1,11 @@
+#Operadores Logicos
+
+x = TRUE
+Y = FALSE
+
+print( x & y );
+print( x && y );
+print( x | y );
+print( x || y );
+print( !x );
+print( !y );
